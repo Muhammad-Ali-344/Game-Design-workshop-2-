4e655644566e6489da67cc8529f78d158d384c42
Muhammad-Ali-344 <145254483+Muhammad-Ali-344@users.noreply.github.com>
2023-09-24 15:50:04 +0500

    first Class

---
+++ first/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class Assignment1
+{
+	public static void main()
+	{
+		String studentname = "ALi";
+		int marks = 91; ;
+		bool ispass = true;
+		float Gpa = 4.0;
+		String studentname = "Saad";
+		int marks = 80;
+		bool ispass = true;
+		float Gpa = 4.0;
+		String studentname = "Usman";
+		int marks = 78;
+		bool ispass = true;
+		float Gpa = 3.8;
+		String studentname = "Zain";
+		int marks = 75;
+		bool ispass = true;
+		float Gpa = 3.5;
+		String studentname = "Aqib";
+		int marks = 71;
+		bool ispass = true;
+		float Gpa = 3.1;
+		String studentname = "Umer";
+		int marks = 68;
+		bool ispass = true;
+		float Gpa = 2.8;
+		String studentname = "Tayyab";
+		int marks = 65;
+		bool ispass = true;
+		float Gpa = 2.5;
+		String studentname = "Moiz";
+		int marks = 62;
+		bool ispass = true;
+		float Gpa = 2.2;
+		String studentname = "Aqib";
+		int marks = 50;
+		bool ispass = true;
+		float Gpa = 1.0;
+		String studentname = "Saim";
+		int marks = 45;
+		bool ispass = false;
+		float Gpa = 0;
+		String studentname = "Hammad";
+		int marks = 40;
+		bool ispass = false;
+		float Gpa = 0;
+
+	}
+}
