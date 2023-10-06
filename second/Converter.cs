ce1a5073ec519d1a9bafc185a9f254780ebc56d8
Muhammad-Ali-344 <145254483+Muhammad-Ali-344@users.noreply.github.com>
2023-10-06 20:49:47 +0500

    Methods

---
+++ second/Converter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Converter
+{
+    public static void Main()
+
+    {
+        public static void FeetToMeter(float num1)
+        {
+            float Ans = num1 / 3.28;
+            Console.WriteLine("Ans: " + Ans);
+        }
+
+        public static void MeterToFeet(float num1)
+        {
+            float Ans = num1 * 0.3048;
+            Console.WriteLine("Ans: " + Ans);
+        }
+
+        public static void MeterToKiloMeter(float num1)
+        {
+            float Ans = num1 / 1000;
+            Console.WriteLine("Ans: " + Ans);
+        }
+
+        public static void kilometersToMeter(float num1)
+        {
+            float Ans = num1 * 1000;
+            Console.WriteLine("Ans: " + Ans);
+        }
+        public static void MilesTokilometers(float num1)
+        {
+            float Ans = num1 * 1.6093;
+            Console.WriteLine("Ans: " + Ans);
+        }
+        public static void kilometersToMiles(float num1)
+        {
+            float Ans = num1 / 1.6093;
+            Console.WriteLine("Ans: " + Ans);
+        }
+        public static void MiliMetersToMeters(float num1)
+        {
+            float Ans = num1 / 1000; ;
+            Console.WriteLine("Ans: " + Ans);
+        }
+        public static void CentimetersToMeters(float num1)
+        {
+            float Ans = num1 / 100;
+            Console.WriteLine("Ans: " + Ans);
+        }
+        public static void MetersToCentimeters(float num1)
+        {
+            float Ans = num1 * 100;
+            Console.WriteLine("Ans: " + Ans);
+        }
+        public static void FeetsToCentimeters(float num1)
+        {
+            float Ans = num1 * 30.48;
+            Console.WriteLine("Ans: " + Ans);
+        }
+    }
+}
+
